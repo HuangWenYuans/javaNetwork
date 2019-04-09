b09ad708a7d8da4724cff5019ba99c75d31f7541
S_16201213 <79868058@qq.com>
2019-04-09 20:40:56 +0800

    第五次实验

---
+++ src/javanet/l05/BusSimulator.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2019
+ * FileName: BusSimulator
+ * Author:   huangwenyuan
+ * Date:     2019/04/09 下午 07:33
+ * Description:
+ */
+
+package javanet.l05;
+
+/**
+ * 功能描述: 公交车模拟程序
+ *
+ * @author huangwenyuan
+ * @create 2019/04/09
+ * @since 1.0.0
+ */
+public class BusSimulator {
+}
+
+    
